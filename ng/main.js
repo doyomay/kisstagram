195ae536d734a5e1ccb3309184214b29fd601f64
Gerardo May <gerardomay@live.com>
2015-06-13 22:48:59 -0500

    - Agregado home. - Agregada animaciones de los post

--- ng/main.js
+++ ng/main.js
@@ -1,6 +1,34 @@
 /**
  * Created by Gerardo on 6/13/2015.
  */
-window.onload= function() {
-	console.log('Hola a todos!');
+var Posts = function () {
+	this.postContainer = null;
+	this.detectPosts = function () {
+		this.postContainer = document.getElementsByClassName('Post');
+	};
+
+	this.addDetectionTop = function () {
+		this.detectPosts();
+		var size, element = null;
+		var altura = window.screen.height;
+		for (var i in this.postContainer) {
+			if (typeof(this.postContainer[i]) == 'object') {
+				element = this.postContainer[i];
+				size = element.getBoundingClientRect();
+				if (size.top < altura && size.top > 0 && size.bottom < altura && size.bottom > 0) {
+					element.classList.add('Post-animar');
+				}else{
+					element.classList.remove('Post-animar');
+				}
+			}
+		}
+	};
 };
+function init() {
+	var Post = new Posts();
+	Post.addDetectionTop();
+	window.onscroll = function () {
+		Post.addDetectionTop();
+	};
+}
+window.onload = init();
