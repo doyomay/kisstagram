cced3f7ab91bfb1c9a3a30b578f2dfc0892c0b4c
Gerardo May <gerardomay@live.com>
2015-06-14 22:45:18 -0500

    - Agregaado metaviewport - Agregada nuevas imagenes de prueba

--- ng/timeline.ctrl.js
+++ ng/timeline.ctrl.js
@@ -7,7 +7,7 @@ angular.module('app')
 		angular.element(document).ready(function(){
 			Post.addDetectionTop();
 		});
-		angular.element(document).bind('scroll',function(){
+		angular.element(document).bind('resize scroll',function(){
 			Post.addDetectionTop();
 		});
 		var postsContainer = [{
@@ -26,7 +26,7 @@ angular.module('app')
 				nombre: 'doyomay',
 				avatar: 'profile.gif'
 			},
-			imagen: 'https://ununsplash.imgix.net/photo-1422207134147-65fb81f59e38?fit=crop&fm=jpg&h=800&q=75&w=1050',
+			imagen: 'https://ununsplash.imgix.net/photo-1429277096327-11ee3b761c93?q=75&fm=jpg&w=1080&fit=max&s=79830900ab0bfa424f02f49d12de4e3c',
 			texto: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci culpa eveniet fuga pariatur perferendis, sint veniam vero! Dolores, earum sequi? Debitis distinctio in laudantium minus necessitatibus obcaecati omnis possimus, suscipit.',
 			comments: [{
 				usuario: 'Gerardo',
@@ -37,7 +37,7 @@ angular.module('app')
 				nombre: 'doyomay',
 				avatar: 'profile.gif'
 			},
-			imagen: 'https://ununsplash.imgix.net/photo-1422207134147-65fb81f59e38?fit=crop&fm=jpg&h=800&q=75&w=1050',
+			imagen: 'https://unsplash.imgix.net/photo-1424746219973-8fe3bd07d8e3?q=75&fm=jpg&w=1080&fit=max&s=1540f8eba1e1efc938d0b00e0c16b86c',
 			texto: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci culpa eveniet fuga pariatur perferendis, sint veniam vero! Dolores, earum sequi? Debitis distinctio in laudantium minus necessitatibus obcaecati omnis possimus, suscipit.',
 			comments: [{
 				usuario: 'Gerardo',
@@ -48,7 +48,7 @@ angular.module('app')
 				nombre: 'doyomay',
 				avatar: 'profile.gif'
 			},
-			imagen: 'https://ununsplash.imgix.net/photo-1422207134147-65fb81f59e38?fit=crop&fm=jpg&h=800&q=75&w=1050',
+			imagen: 'https://ununsplash.imgix.net/photo-1430126833726-4a091c572f3c?q=75&fm=jpg&w=1080&fit=max&s=e3c32f5602ca5b31e5b32fdea80f3e5d',
 			texto: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci culpa eveniet fuga pariatur perferendis, sint veniam vero! Dolores, earum sequi? Debitis distinctio in laudantium minus necessitatibus obcaecati omnis possimus, suscipit.',
 			comments: [{
 				usuario: 'Gerardo',
